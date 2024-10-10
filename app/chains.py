373cec271e5a1c95383d96fab9c771e99007eccc
kartikgupta14 <119725053+kartikgupta14@users.noreply.github.com>
2024-10-10 10:17:29 +0530

    Add files via upload

---
+++ app/chains.py
@@ -0,0 +1,61 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:  
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Kartik Gupta, a final year Computer Science student at J.C. Bose University of Science and Technology .
+        You are Highly motivated and dedicated software engineering student with a strong passion for learning and continuous improvement. A
+        proactive team player who is eager to contribute to innovative projects and make a positive impact in the field of software engineering having skills of Machine Learning, Web Development, Mobile Development and Data Structures and Algorithms.
+        Your job is to write a cold email to the client regarding the job mentioned above describing the capability of yourself
+        in fulfilling their needs.
+        Also add the most relevant ones from the following links to showcase your portfolio: {link_list}
+        Remember you Kartik Gupta.
+        Do not provide a preamble.
+        ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
